e0c75eb6f381a81a9a76e60d0588ae6a6c88c9df
Aarush Narang <70560514+aarush-narang@users.noreply.github.com>
2023-05-02 19:17:55 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
     return <>Hello World</>;
 }
-
-// Introduction page, header and footer
-// Header will contain sign in button
-// Footer will contain other links: about, contact, github, created with Next.js, etc.
