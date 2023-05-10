6061142a9050315b54f49ec12f786cef7d3ffa15
aarush-narang <aarushnarang@gmail.com>
2023-05-09 22:13:24 -0700

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,7 +2,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
     siteUrl:
-        process.env.NODE_ENV === 'production' ? 'https://www.enrichedcs.org' : 'http://localhost:3000',
+        process.env.NODE_ENV === 'production' ? 'https://www.example.com' : 'http://localhost:3000',
     generateRobotsTxt: true, // (optional)
     // ...other options
 };
